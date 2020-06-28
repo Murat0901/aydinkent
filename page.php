880c894fa0484600f0799a24e97f81ca26945edc
unknown <menzilcim@gmail.com>
2020-06-28 16:05:33 +0300

    home and about page fixes

--- page.php
+++ page.php
@@ -1,16 +1,25 @@
 <?php get_header(); ?>
 
-<div class="container">
-    <h1><?php the_title() ?></h1>
+<div class="page-post">
+    <div class="overlay"></div>
+    <div class="page-top">
+        <?php if (has_post_thumbnail()) : ?>
+            <img src="<?php the_post_thumbnail_url('full'); ?>" class="img-fluid">
+        <?php endif; ?>
+    </div>
+    <div class="container p-3">
 
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <h1><?php the_title() ?></h1>
 
-            <?php the_content() ?>
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+                <?php the_content() ?>
 
 
-    <?php endwhile;
-    endif ?>
+        <?php endwhile;
+        endif ?>
 
+    </div>
 
 </div>
 
